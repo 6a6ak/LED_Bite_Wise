ac1d413bf0d43c22a16d03e7f6275bc02a251e75
6a6ak <babak.ahari@gmail.com>
2025-05-16 00:49:38 +0200

    Implement code changes to enhance functionality and improve performance

--- main.c
+++ main.c
@@ -7,18 +7,25 @@
 #define BAUD 9600
 #define MYUBRR (F_CPU/16/BAUD - 1)
 
-#define TRIG_PIN PE4  // pin 2 -> Trigger pin -d2-> PORT E 4th bit 
-#define ECHO_PIN PE5  // pin 3 -> Echo pin -d3-> PORT E 5th bit
-// HC-SR04 pins on Arduino Mega in Microcontroler ATMega 2560 placed on PORT E & not on PORT D
-#define TRIG_PIN PE4  // pin 2 -> Trigger pin -d2-> PORT E 4th bit 
-#define ECHO_PIN PE5  // pin 3 -> Echo pin -d3-> PORT E 5th bit
+#define PIN2 PE4  // pin 2 -> Trigger pin -d2-> PORT E 4th bit 
+#define PIN3 PE5  // pin 3 -> Echo pin -d3-> PORT E 5th bit
+
 int main(void) {
     // تنظیم همه پین‌های PORT E به عنوان خروجی
     DDRE = 0xFF; // همه بیت‌ها خروجی
 
+    // Set PE3 and PE5 as output (LED3 and LED1)
+    DDRE |= (1 << PE3) | (1 << PE5);
+    // Set PH4 as output (LED2)
+    DDRH |= (1 << PH4);
+
     while (1) {
         // روشن کردن فقط بیت 0 (PE0)
         PORTE |= (1 << PE0);
+        _delay_ms(500);
+
+            // روشن کردن فقط بیت 0 (PE0)
+        PORTE |= (1 << PE7);
         _delay_ms(500);
 
         // خاموش کردن فقط بیت 0 (PE0)
@@ -40,6 +47,22 @@ int main(void) {
 
         PORTE &= ~(1 << PE4); // خاموش کردن PE4
         PORTE |= (1 << PE5); // روشن کردن PE5
+        _delay_ms(500);
+
+        // فقط روشن و خاموش کردن PE7
+        PORTE |= (1 << PH4); // روشن کردن PE7
+        _delay_ms(1000);
+        PORTE &= ~(1 << PH4); // خاموش کردن PE7
+        _delay_ms(1000);
+
+        // Turn ON all LEDs
+        PORTE |= (1 << PE3) | (1 << PE5);
+        PORTH |= (1 << PH4);
+        _delay_ms(500);
+
+        // Turn OFF all LEDs
+        PORTE &= ~((1 << PE3) | (1 << PE5));
+        PORTH &= ~(1 << PH4);
         _delay_ms(500);
     }
 }
