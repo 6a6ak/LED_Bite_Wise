394a6d3c3cb44f9dc11e86f6ee077e2d95fae260
6a6ak <babak.ahari@gmail.com>
2025-05-16 00:06:19 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#define F_CPU 16000000UL  // 16MHz clock speed
+#include <avr/io.h> //Rigister sssumption like DDRx, PORTx, PINx
+#include <util/delay.h> // For delay functions
+#include <stdlib.h> // For itoa function convert integer to string
+
+// UART config
+#define BAUD 9600
+#define MYUBRR (F_CPU/16/BAUD - 1)
+
+#define TRIG_PIN PE4  // pin 2 -> Trigger pin -d2-> PORT E 4th bit 
+#define ECHO_PIN PE5  // pin 3 -> Echo pin -d3-> PORT E 5th bit
+// HC-SR04 pins on Arduino Mega in Microcontroler ATMega 2560 placed on PORT E & not on PORT D
+#define TRIG_PIN PE4  // pin 2 -> Trigger pin -d2-> PORT E 4th bit 
+#define ECHO_PIN PE5  // pin 3 -> Echo pin -d3-> PORT E 5th bit
+int main(void) {
+    // Configure TRIG as output, ECHO as input
+    DDRE |= (1 << TRIG_PIN);
+    DDRE &= ~(1 << ECHO_PIN);
+
+ 
+    }
