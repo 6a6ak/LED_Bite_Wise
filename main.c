01e296873589cd2d9bc63215642dc0a6c424139d
6a6ak <babak.ahari@gmail.com>
2025-05-16 16:15:50 +0200

    Refactor code structure for improved readability and maintainability

--- main.c
+++ main.c
@@ -1,68 +1,27 @@
-#define F_CPU 16000000UL  // 16MHz clock speed
-#include <avr/io.h> //Rigister sssumption like DDRx, PORTx, PINx
-#include <util/delay.h> // For delay functions
-#include <stdlib.h> // For itoa function convert integer to string
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
 
-// UART config
-#define BAUD 9600
-#define MYUBRR (F_CPU/16/BAUD - 1)
-
-#define PIN2 PE4  // pin 2 -> Trigger pin -d2-> PORT E 4th bit 
-#define PIN3 PE5  // pin 3 -> Echo pin -d3-> PORT E 5th bit
-
+#define LED1 PE4 // Pin 2
 int main(void) {
-    // تنظیم همه پین‌های PORT E به عنوان خروجی
-    DDRE = 0xFF; // همه بیت‌ها خروجی
-
-    // Set PE3 and PE5 as output (LED3 and LED1)
-    DDRE |= (1 << PE3) | (1 << PE5);
-    // Set PH4 as output (LED2)
-    DDRH |= (1 << PH4);
+    // Set PE3, PE4, and PE5 as output (pins 2, 3, and 5)
+    DDRE |= (1 << PE3) | (1 << PE4) | (1 << PE5);
+    // Set PH3 and PH4 as output (pins 6 and 7)
+    DDRH |= (1 << PH3) | (1 << PH4);
+    // Set PG5 as output (pin 4)
+    DDRG |= (1 << PG5);
 
     while (1) {
-        // روشن کردن فقط بیت 0 (PE0)
-        PORTE |= (1 << PE0);
+        // Turn ON all pins 2 to 7
+        PORTE |= (1 << PE3) | (1 << PE4) | (1 << PE5); // 00000000 and xxxxxxx1
+        PORTH |= (1 << PH3) | (1 << PH4);
+        PORTG |= (1 << PG5);
         _delay_ms(500);
 
-            // روشن کردن فقط بیت 0 (PE0)
-        PORTE |= (1 << PE7);
-        _delay_ms(500);
-
-        // خاموش کردن فقط بیت 0 (PE0)
-        PORTE &= ~(1 << PE0);
-        _delay_ms(500);
-
-        // روشن کردن همه بیت‌ها
-        PORTE = 0xFF;
-        _delay_ms(500);
-
-        // خاموش کردن همه بیت‌ها
-        PORTE = 0x00;
-        _delay_ms(500);
-
-        // روشن و خاموش کردن بیت 4 و 5 (مثلاً برای TRIG و ECHO)
-        PORTE |= (1 << PE4); // روشن کردن PE4
-        PORTE &= ~(1 << PE5); // خاموش کردن PE5
-        _delay_ms(500);
-
-        PORTE &= ~(1 << PE4); // خاموش کردن PE4
-        PORTE |= (1 << PE5); // روشن کردن PE5
-        _delay_ms(500);
-
-        // فقط روشن و خاموش کردن PE7
-        PORTE |= (1 << PH4); // روشن کردن PE7
-        _delay_ms(1000);
-        PORTE &= ~(1 << PH4); // خاموش کردن PE7
-        _delay_ms(1000);
-
-        // Turn ON all LEDs
-        PORTE |= (1 << PE3) | (1 << PE5);
-        PORTH |= (1 << PH4);
-        _delay_ms(500);
-
-        // Turn OFF all LEDs
-        PORTE &= ~((1 << PE3) | (1 << PE5));
-        PORTH &= ~(1 << PH4);
+        // Turn OFF all pins 2 to 7
+        PORTE &= ~((1 << PE3) | (1 << PE4) | (1 << PE5));
+        PORTH &= ~((1 << PH3) | (1 << PH4));
+        PORTG &= ~(1 << PG5);
         _delay_ms(500);
     }
 }
