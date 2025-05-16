435e3478eaf087619563c978b8e51a24bd4fc55c
6a6ak <babak.ahari@gmail.com>
2025-05-16 22:36:35 +0200

    Update LED control logic and regenerate binary files

--- main.c
+++ main.c
@@ -2,26 +2,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define LED1 PE4 // Pin 2
+#define LED1 PE5
+ // Pin 2
 int main(void) {
     // Set PE3, PE4, and PE5 as output (pins 2, 3, and 5)
-    DDRE |= (1 << PE3) | (1 << PE4) | (1 << PE5);
-    // Set PH3 and PH4 as output (pins 6 and 7)
-    DDRH |= (1 << PH3) | (1 << PH4);
-    // Set PG5 as output (pin 4)
-    DDRG |= (1 << PG5);
+    DDRE |= (1 << LED1);
+
 
     while (1) {
         // Turn ON all pins 2 to 7
-        PORTE |= (1 << PE3) | (1 << PE4) | (1 << PE5); // 00000000 and xxxxxxx1
-        PORTH |= (1 << PH3) | (1 << PH4);
-        PORTG |= (1 << PG5);
+        PORTE |= (1 << LED1);
         _delay_ms(500);
 
         // Turn OFF all pins 2 to 7
-        PORTE &= ~((1 << PE3) | (1 << PE4) | (1 << PE5));
-        PORTH &= ~((1 << PH3) | (1 << PH4));
-        PORTG &= ~(1 << PG5);
+        PORTE &= ~(1 << LED1);
         _delay_ms(500);
     }
 }
